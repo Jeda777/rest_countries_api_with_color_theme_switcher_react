824d44bd39ba9e2dd2a2a96a1ff9147cac98580b
Jeda777 <jakub.jeda@wp.pl>
2022-12-13 21:31:56 +0100

    Navbar style update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,8 @@ const Navbar = ({ darkMode, setDarkMode }) => {
       <div className='custom-container py-8 flex justify-between'>
         <h1 className='font-extrabold text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl'>Where in the world?</h1>
         <div
-          className='flex items-center gap-1 sm:gap-2 lg:gap-3 cursor-pointer text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl'
+          className='flex items-center gap-1 sm:gap-2 lg:gap-3 cursor-pointer text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl
+          transition-all duration-300 hover:opacity-50'
           onClick={() => setDarkMode((prev) => !prev)}
         >
           {darkMode ? <HiMoon /> : <HiOutlineMoon />}
